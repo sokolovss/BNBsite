0453fde608266c5417783342c8fb97d6b39a3b0d
Sergey Sokolov <sokolovs@gmail.com>
2021-05-03 22:32:07 -0700

    pat to chi replacement

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/sokolovss/BNBsite/pkg/config"
 	"github.com/sokolovss/BNBsite/pkg/handlers"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewMux()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
