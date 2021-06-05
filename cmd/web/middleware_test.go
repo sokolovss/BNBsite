e966abdf04e69f4be2653cac7e4affe32ecaa274
Sergey Sokolov <sokolovs@gmail.com>
2021-06-04 18:51:42 -0700

    noSurf tests

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -1,7 +1,19 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 func TestNoSurf(t *testing.T) {
+	var testH TestHandler
 
+	h := NoSurf(&testH)
+
+	switch h.(type) {
+	case http.Handler:
+	//pass
+	default:
+		t.Error("Type is not http.Handler")
+	}
 }
--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+
+	os.Exit(m.Run())
+}
+
+type TestHandler struct{}
+
+func (t *TestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
