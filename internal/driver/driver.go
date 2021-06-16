c153a55c745bd03971847579d84f6efb12b4c91c
Sergey Sokolov <sokolovs@gmail.com>
2021-06-15 19:36:46 -0700

    DB driver package

---
+++ internal/driver/driver.go
@@ -0,0 +1,49 @@
+package driver
+
+import (
+	"database/sql"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"log"
+	"time"
+)
+
+//DB holds the Database connection pool
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDBConn = 10
+const maxIdleDBConn = 5
+const maxDBConnLifeTime = 5 * time.Minute
+
+//ConnectSQL creates DB pool for postgres
+func ConnectSQL(dsn string) (*DB, error) {
+	d, err := NewDatabase(dsn)
+	if err != nil {
+		log.Fatalln("Cannot connect to the database\n", err)
+	}
+	d.SetMaxOpenConns(maxOpenDBConn)
+	d.SetMaxIdleConns(maxIdleDBConn)
+	d.SetConnMaxLifetime(maxDBConnLifeTime)
+
+	dbConn.SQL = d
+
+	return dbConn, nil
+
+}
+
+//NewDatabase creates a new database for the application
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
