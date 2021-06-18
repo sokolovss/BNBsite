d0e144c727b81baebce50cf24069534c8d27008f
Sergey Sokolov <sokolovs@gmail.com>
2021-06-17 21:52:03 -0700

    Models add

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,31 @@
 package models
 
+import "time"
+
 //Reservation model for reservation data
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+//Users is a model for users table
+type User struct {
+	ID           int
+	FirstName    string
+	LastName     string
+	Email        string
+	Password     string
+	access_level int
+	created_at   time.Time
+	updated_at   time.Time
+}
+
+//Rooms is a model for users table
+type Rooms struct {
+	ID         int
+	RoomName   string
+	created_at time.Time
+	updated_at time.Time
 }
