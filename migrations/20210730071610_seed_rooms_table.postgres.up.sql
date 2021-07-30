e24f4369f4ecd5f00329b03663b32b1e9de36967
Sergey Sokolov <sokolovs@gmail.com>
2021-07-30 00:19:25 -0700

    rooms seeding migrations

---
+++ migrations/20210730071610_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."rooms" ("id", "room_name", "created_at", "updated_at") VALUES
+                                                                                 (1, 'General''s Room', '2021-06-22 00:00:00', '2021-06-22 00:00:00'),
+                                                                                 (2, 'Colonel''s room', '2021-07-05 00:00:00', '2021-07-05 00:00:00');
